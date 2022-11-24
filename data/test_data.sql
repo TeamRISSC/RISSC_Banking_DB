b1bddfe9145343baa246e365b33b8ffe795a3346
Isuru Gunarathne <isuru623@gmail.com>
2022-11-24 10:37:16 +0530

    insert FD and online loans

--- data/test_data.sql
+++ data/test_data.sql
@@ -23,18 +23,31 @@ insert into branch values ('006', 'Kurunegala', '600, Kurunegala', '00006');
 insert into branch values ('007', 'Gampaha', '700, Gampaha', '00007');
 
 -- Insert customers
-insert into customer values ('00001', 'Savings', 'Chathura', '001, Kandy', \
+insert into customer values ('00001', 'Individual', 'Chathura', '001, Kandy', \
                              '076652613', 'Chathura', '12345');
-insert into customer value ('00002', 'Savings', 'Kasun', '002, Colombo', \
+insert into customer value ('00002', 'Individual', 'Kasun', '002, Colombo', \
                             '076652614', 'Kasun', '12345');
-insert into customer value ('00003', 'Savings', 'Nimal', '003, Galle', \
+insert into customer value ('00003', 'Organisation', 'Nimal pvt ltd', '003, Galle', \
                             '076652615', 'Nimal', '12345');
 
+-- Insert fixed deposits
+insert into fixed_deposit values ('00001', '00001', '00001', 10000, 1, 5, \
+                                  '2023-12-31');
+insert into fixed_deposit values ('00002', '00002', '00002', 20000, 2, 5, \
+                                    '2023-12-31');
+   
+
+-- Insert online_loans 
+insert into online_loan values ('00001', '00001', '00001','00001',10000,'2022-11-23', 5);
+insert into online_loan values ('00002', '00002', '00002','00002',20000,'2022-11-23', 5);
+
 -- Insert bank_accounts
-insert into bank_account values ('00001', '00001', '1000.00', 'Savings', \
-                                 '2017-01-01', '2017-01-01', '2017-01-01');
-insert into bank_account values ('00002', '00002', '2000.00', 'Savings', \
-                                    '2017-01-01', '2017-01-01', '2017-01-01');  
-insert into bank_account values ('00003', '00003', '3000.00', 'Savings', \
-                                    '2017-01-01', '2017-01-01', '2017-01-01');  
+insert into bank_account values ('00001', '00001', '001', 'Chathura', 10000, 1000, 'Savings', 5,10,2);
+insert into bank_account values ('00002', '00002', '002', 'Kasun', 20000, 1000, 'Savings', 5,10,2);
                                     
+-- Insert transfers
+insert into transfer values ('12345', '00001', '00002', '2017-01-01', 1000.00,"opening account");
+insert into transfer values ('12346', '00002', '00001', '2017-01-01', 2000.00,"returning open account amount");
+insert into transfer values ('12347', '00001', '00003', '2017-01-01', 1000.00,"education expense");
+
+-- Insert deposits
