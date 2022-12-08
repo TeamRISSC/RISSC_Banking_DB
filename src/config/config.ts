effe772cea44c3816a0b6c0fc3c599cf40b63625
PRO-GUNE <chathuragunasekera@gmail.com>
2022-12-08 09:27:13 +0530

    dummy configurations

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,9 @@
 // Contains the configurations for the server
+let configurations = {
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"db"
+};
+
+export {configurations};
