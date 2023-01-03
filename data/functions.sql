16d76ad967f397eeefb13bc9ee1413b5ec3b3677
PRO-GUNE <chathuragunasekera@gmail.com>
2023-01-03 19:48:13 +0530

    functions.sql updated with check balance

--- data/functions.sql
+++ data/functions.sql
@@ -1,40 +1,15 @@
-CREATE FUNCTION transfer_funds(
-    fromAccount INT, toAccount INT, amount DECIMAL,
-    date DATETIME, remarks VARCHAR(50))
-RETURNS INT
+CREATE DEFINER=`admin`@`localhost` FUNCTION `check_balance`(account_number VARCHAR(10), amount DECIMAL) RETURNS int
+    DETERMINISTIC
 BEGIN
-  DECLARE numRows INT DEFAULT 0;
+  DECLARE acc_balance, min_balance DECIMAL;
+  -- Get the balance of the account
   
-  -- Start a transaction
-  START TRANSACTION;
+  SELECT balance, minBalance INTO acc_balance, min_balance 
+  FROM bank_account WHERE accountNumber = account_number;
   
-  -- Update the balance of the fromAccount
-  UPDATE accounts
-  SET balance = balance - amount
-  WHERE account_number = fromAccount;
-  
-  -- Update the balance of the toAccount
-  UPDATE accounts
-  SET balance = balance + amount
-  WHERE account_number = toAccount;
-  
-  -- Insert a new row into the transactions table
-  INSERT INTO transactions SET
-    from_account = fromAccount,
-    to_account = toAccount,
-    amount = amount,
-    transaction_time = NOW();
-  
-  -- Get the number of rows affected by the UPDATE and INSERT statements
-  SELECT ROW_COUNT() INTO numRows;
-  
-  -- If both UPDATE and INSERT statements were successful, commit the transaction
-  IF numRows = 3 THEN
-    COMMIT;
-  ELSE
-    -- Otherwise, roll back the transaction
-    ROLLBACK;
+  -- If the balance is not sufficient, throw an error
+  IF (acc_balance - amount) < min_balance THEN
+    RETURN -1;
   END IF;
-  
-  RETURN numRows;
+  RETURN 1;
 END
