c5b3185d255e01d88e0c9874075c8165e5cf97a9
Supun Gamlath <supungamlath@outlook.com>
2023-01-05 00:55:24 +0530

    Add port to database service config

--- src/services/database.js
+++ src/services/database.js
@@ -10,6 +10,7 @@ const {admin_config} = require('../../src/config/config')
   async init(config=admin_config){
     this.connection = await mysql.createConnection({
       host: config.host,
+      port: config.port,
       user: config.user,
       password: config.password,
       database: config.database
