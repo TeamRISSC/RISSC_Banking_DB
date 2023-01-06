5b004b7b5435727210d9d9ca2890f8a124f1af36
Isuru Gunarathne <isuru623@gmail.com>
2023-01-06 09:19:16 +0530

    revert back to local host

--- src/config/config.js
+++ src/config/config.js
@@ -7,39 +7,35 @@ const azure_config = {
     database:"localdb"
 }
 const admin_config = {
-    host:"127.0.0.1",
-    port: process.env.WEBSITE_MYSQL_PORT || 51940,
+    host:"localhost",
     user:"admin",
     password:"admin",
     database:"bank"
 }
 
 const manager_config = {
-    host:"127.0.0.1",
-    port: process.env.WEBSITE_MYSQL_PORT || 51940,
+    host:"localhost",
     user:"manager",
     password:"manager",
     database:"bank"
 }
 
 const employee_config = {
-    host:"127.0.0.1",
-    port: process.env.WEBSITE_MYSQL_PORT || 51940,
+    host:"localhost",
     user:"employee",
     password:"employee",
     database:"bank"
 }
 
 const customer_config = {
-    host:"127.0.0.1",
-    port: process.env.WEBSITE_MYSQL_PORT || 51940,
+    host:"localhost",
     user:"customer",
     password:"customer",
     database:"bank"
 }
 
 const server_config = {
-    port: process.env.PORT || 5000
+    port:5000
 }
 
 const secret = 'secret'
--- src/services/database.js
+++ src/services/database.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2/promise');
 const {azure_config} = require('../../src/config/config') 
+const {admin_config} = require('../../src/config/config')
 
 
  class MySQLDBMySQLDB {
@@ -7,8 +8,13 @@ const {azure_config} = require('../../src/config/config')
      this.init(config)
   }
 
-  async init(config=azure_config){
-    this.connection = await mysql.createConnection(azure_config);
+  async init(config=admin_config){
+    this.connection = await mysql.createConnection({
+      host: config.host,
+      user: config.user,
+      password: config.password,
+      database: config.database
+    });
   }
 }
 
