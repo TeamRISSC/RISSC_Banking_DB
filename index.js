9d9d9c26af9332abe3a64c261c71686721f04943
Supun Gamlath <supungamlath@outlook.com>
2023-01-07 19:17:27 +0530

    Add CORS policy

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ const port = server_config.port
 
 // Use the body-parser middleware to parse the request body
 app.use(bodyParser.json());
+// Accept these headers to avoid CORS errors on the client side
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Default Route
 app.get('/', (req, res) => res.send("Welcome to RISSC Banking API"))
